ba5e3be157902d5ec817bf6b8e56268a28f9825f
Jenny <jennyberry2809@gmail.com>
2023-10-29 15:20:01 +0100

    commit

--- _hw/src/main/java/_hw_1/ShopManager.java
+++ _hw/src/main/java/_hw_1/ShopManager.java
@@ -5,10 +5,17 @@ public class ShopManager {
     private Product[] products;
     private int productCount;
 
+
+    public ShopManager(int maxProduct) {
+        products = new Product[maxProduct];
+        productCount = 0;
+    }
+
     public void addProduct(Product product) {
         if (productCount < products.length) {
             products[productCount] = product;
             productCount++;
+            System.out.println("Товар " + product.name + " " + product.category + " " + product.price + " " + product.currency + " добавлен");
         } else {
             System.out.println("Достигнуто максимальное количество товаров");
         }
@@ -36,6 +43,24 @@ public class ShopManager {
         }
 
         System.out.println(result);
+    }
+}
+
+class ShopManagerTest {
+
+    public static void main(String[] args) {
+
+        ShopManager shopManager = new ShopManager(6);
+
+        shopManager.addProduct(new Product(1, "Levis", ProductCategory.JEANS, 12.99, Currency.USD));
+        shopManager.addProduct(new Product(2, "Diesel", ProductCategory.JEANS, 24.99, Currency.USD));
+        shopManager.addProduct(new Product(3, "Bershka", ProductCategory.JACKETS, 29.99, Currency.EUR));
+        shopManager.addProduct(new Product(4, "ZARA", ProductCategory.DRESSES, 28.99, Currency.GPB));
+        shopManager.addProduct(new Product(5, "O`stin", ProductCategory.JACKETS, 34.99, Currency.EUR));
+        shopManager.addProduct(new Product(6, "H&M", ProductCategory.DRESSES, 14.99, Currency.USD));
+        shopManager.addProduct(new Product(7, "KiK", ProductCategory.JACKETS, 17.99, Currency.GPB));
+
+
     }
 
 }
